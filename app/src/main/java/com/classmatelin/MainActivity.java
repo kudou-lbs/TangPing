8065f27a93e039d2ed74cbf499a1090fb989bd09
lbs <3514633170@qq.com>
2022-03-10 17:30:13 +0800

    init

---
+++ app/src/main/java/com/classmatelin/MainActivity.java
@@ -0,0 +1,462 @@
+package com.classmatelin;
+
+import android.Manifest;
+import android.app.ActivityManager;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.media.AudioManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.provider.ContactsContract;
+import android.telephony.SmsManager;
+import android.util.AndroidRuntimeException;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.baidu.speech.EventListener;
+import com.baidu.speech.EventManager;
+import com.baidu.speech.EventManagerFactory;
+import com.baidu.speech.asr.SpeechConstant;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity{
+    //联系人信息
+    public class ContactInfo {
+        private String name;
+        private String number;
+        public  ContactInfo(String name,String number){
+            this.name=name;
+            this.number=number;
+        }
+
+        public String getName(){return name;}
+        public String getNumber(){return number;}
+    }
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    protected TextView txtResult;//识别结果
+    protected FloatingActionButton microphone;//语音按钮
+
+    private String res;//语音识别结果
+    private String msg_number;//短信联系人电话
+    private String msg_name=null;//联系人姓名
+    private boolean isMessage=false;//是否是即将发送的消息内容
+
+    private long eventtime = 0;
+    private AudioManager vAudioManager = null;
+
+    private EventManager asr;//语音识别核心库
+    private EventManager wp;//语音唤醒
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //将窗口设置在底部
+        getWindow().getAttributes().width = ActionBar.LayoutParams.MATCH_PARENT;
+        getWindow().getAttributes().gravity = Gravity.BOTTOM;
+
+        //初始化控件
+        initView();
+
+        //获取权限
+        initPermission();
+
+        //初始化EventManager对象
+        asr = EventManagerFactory.create(this, "asr");
+        //注册自己的输出事件类
+        asr.registerListener(MyListener); //EventListener 中 onEvent方法
+
+        //初始化EventManager对象
+        wp = EventManagerFactory.create(this, "wp");
+        //注册监听事件
+        wp.registerListener(WakeUpListener);//EventListener 中 onEvent方法
+
+        if(isServiceRunning("com.classmatelin.wakeUpService")){
+            Log.d(TAG,"服务早已存在，不作启动");
+        }else{
+            Intent startWakeUpService = new Intent(MainActivity.this,wakeUpService.class);
+            startService(startWakeUpService);
+        }
+    }
+
+
+    //判断服务是否运行（确保每次只调用一次startCommand()）
+    private boolean isServiceRunning(final String className){
+        ActivityManager activityManager=(ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> info=activityManager.getRunningServices(Integer.MAX_VALUE);
+        if(info==null||info.size()==0) return false;
+        for(ActivityManager.RunningServiceInfo serviceInfo:info){
+            if(className.equals(serviceInfo.service.getClassName())) return true;
+        }
+        return false;
+    }
+
+
+    //下面是classmateLin的旧代码，躺屏的新代码放在这里上面onCreate()下面
+
+    /**
+     * 初始化控件
+     */
+    private void initView() {
+        //获取按钮
+        txtResult = (TextView) findViewById(R.id.tv_txt);
+        microphone = (FloatingActionButton) findViewById(R.id.microphone);
+
+        //设置点击事件
+        microphone.setOnClickListener(new View.OnClickListener() {//开始
+            @Override
+            public void onClick(View v) {
+                asr.send(SpeechConstant.ASR_START, "{}", null, 0, 0);
+            }
+        });
+    }
+
+    /**
+     * android 6.0 以上需要动态申请权限
+     */
+    private void initPermission() {
+        String permissions[] = {Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.INTERNET,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_CONTACTS,
+                Manifest.permission.READ_CONTACTS,
+                Manifest.permission.GET_ACCOUNTS,
+                Manifest.permission.SEND_SMS,//
+                Manifest.permission.READ_SMS,//
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.MODIFY_AUDIO_SETTINGS,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.MEDIA_CONTENT_CONTROL//
+        };
+
+        try {
+            ActivityCompat.requestPermissions(this, permissions,0x0010);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 自定义输出事件类 EventListener 回调方法
+     */
+
+    //唤醒事件
+    EventListener WakeUpListener = new EventListener() {
+        @Override
+        public void onEvent(String name, String params, byte[] bytes, int offset, int length) {
+            try {
+                //解析json文件
+                JSONObject json = new JSONObject(params);
+                if ("wp.data".equals(name)) { // 每次唤醒成功, 将会回调name=wp.data的时间, 被激活的唤醒词在params的word字段
+                    String word = json.getString("word"); // 唤醒词
+                    //这里我们简单打印出唤醒词
+                    Log.d(TAG, word);
+                } else if ("wp.exit".equals(name)) {
+                    // 唤醒已经停止
+                }
+            } catch (JSONException e) {
+                throw new AndroidRuntimeException(e);
+            }
+        }
+    };
+
+    //识别事件
+    EventListener MyListener = new EventListener() {
+        @Override
+        public void onEvent(String name, String params, byte[] data, int offset, int length) {
+
+            if (name.equals(SpeechConstant.CALLBACK_EVENT_ASR_PARTIAL)) {
+                // 识别相关的结果都在这里
+                if (params == null || params.isEmpty()) {
+                    return;
+                }
+                if (params.contains("\"final_result\"")) {
+                    // 一句话的最终识别结果
+                    Log.i("ars.event",params);
+
+                    Gson gson = new Gson();
+                    ASRresponse asRresponse = gson.fromJson(params, ASRresponse.class);//数据解析转实体bean
+
+                    if(asRresponse == null) return;
+                    //从日志中，得出Best_result的值才是需要的，但是后面跟了一个中文输入法下的逗号，
+                    if(asRresponse.getBest_result().contains("，")){//包含逗号  则将逗号替换为空格
+                        txtResult.setText(asRresponse.getBest_result().replace('，',' ').trim());//替换为空格之后，通过trim去掉字符串的首尾空格
+                        res=txtResult.getText().toString();
+                        onActivityResult(res);
+                    }else {//不包含
+                        txtResult.setText(asRresponse.getBest_result().trim());
+                        res=txtResult.getText().toString();
+                        onActivityResult(res);
+                    }
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //发送取消事件
+        asr.send(SpeechConstant.ASR_CANCEL, "{}", null, 0, 0);
+        //退出事件管理器
+        // 必须与registerListener成对出现，否则可能造成内存泄露
+        asr.unregisterListener(MyListener);
+        wp.unregisterListener(WakeUpListener);
+    }
+
+
+    //根据麦克风输入结果执行动作
+    protected void onActivityResult(String result){
+        if(isMessage){
+            sendMessage(result);
+            isMessage=false;
+            msg_number=null;
+            msg_name=null;
+        }
+        if(result.contains("打开")){
+            String appName=result.substring(result.indexOf("开")+1,result.indexOf("。"));
+            Log.d("打开：",appName);
+            //test
+            openApp(appName);
+            return;
+        }
+        if(result.contains("打电话")){
+            call();
+            return;
+        }
+        if(result.contains("发短信")){
+            getSendMsgContactInfo();
+            //发送信息对象有误
+            if(msg_number==null)return;
+
+            txtResult.setText("请输入要发送消息的内容");
+            return;
+        }
+        if(res.contains("搜索")){
+            String searchContent=res.substring(res.indexOf("索")+1);
+            surfTheInternet(searchContent);
+            return;
+        }
+        eventtime = SystemClock.uptimeMillis();
+        vAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        if(res.contains("播放")){
+            //openApp("酷狗音乐");
+            if(vAudioManager.isMusicActive()) return;
+            else playMusic();
+        }
+        if(res.contains("暂停")){
+            pauseMusic();
+        }
+        if(res.contains("下一首")) {
+            nextMusic();
+        }
+        if(res.contains("上一首")){
+            lastMusic();
+        }
+        if(res.contains("音量")) {
+            if (res.contains("降低") || res.contains("调低")) {
+                vAudioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI);
+            }
+            if (res.contains("提高")||res.contains("调高")) {
+                vAudioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
+            }
+        }
+    }
+
+    //发送短信的内容
+    private void sendMessage(String result) {
+        if(msg_number==null) return;
+
+        SmsManager manager=SmsManager.getDefault();
+        manager.sendTextMessage(msg_number,null,result,null,null);
+    }
+
+    //根据语音输入的电话而非姓名进行操作
+    private String getNumber(String result){
+        if(result.contains("给")){
+            String number=res.substring(res.indexOf("给")+1,res.indexOf("。"));
+            for(int i=0;i<number.length();++i){
+                if(!Character.isDigit(number.charAt(i))){
+                    return null;
+                }
+            }
+            return number;
+        }
+        return null;
+    }
+
+    //打开应用
+    private void openApp(String appName){
+        PackageManager packageManager=MainActivity.this.getPackageManager();
+        //获取应用列表
+        List<PackageInfo> packageInfos=packageManager.getInstalledPackages(0);
+        //在应用列表中查询应用
+        for(int i=0;i<packageInfos.size();++i){
+            PackageInfo p=packageInfos.get(i);
+            String label=packageManager.getApplicationLabel(p.applicationInfo).toString();
+            Log.d("tag",label);
+            //查询成功启动应用
+            if(label.equals(appName)){
+                String pName=p.packageName;
+                Intent intent=packageManager.getLaunchIntentForPackage(pName);
+                startActivity(intent);
+                return;
+            }
+        }
+    }
+
+    //获取联系人信息
+    private List<ContactInfo> getContactList(Context context){
+        List<ContactInfo> lists=new ArrayList();
+        Cursor cursor=context.getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null,null,null,null);
+        while (cursor.moveToNext()){
+            //读取通讯录名字
+            String name =cursor.getString(
+                    cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            //读取通讯录号码
+            String number =cursor.getString(
+                    cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            ContactInfo contactInfo=new ContactInfo(name,number);
+            lists.add(contactInfo);
+        }
+        return lists;
+    }
+
+    //打电话
+    private void call(){
+        //获取联系人信息
+        List<ContactInfo> contactInfos=getContactList(this);
+        //信息为空时返回
+        if(contactInfos.isEmpty()){
+            return;
+        }
+        for(ContactInfo contactInfo:contactInfos){
+            if(res.contains(contactInfo.getName())||res.contains(contactInfo.getNumber())){
+                String number=contactInfo.getNumber();
+                Intent intent=new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:"+number));
+                startActivity(intent);
+                return;
+            }
+        }
+        if(res.contains("打电话给")){
+            String number=getNumber(res);
+            if(number==null) return;
+            Intent intent=new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:"+number));
+            startActivity(intent);
+            return;
+        }
+    }
+
+    //发短信给xxx
+    private void getSendMsgContactInfo(){
+        List<ContactInfo> contactInfos=getContactList(this);
+        if(contactInfos.isEmpty()){
+            return;
+        }
+        for(ContactInfo contactInfoL:contactInfos){
+            if(res.contains((contactInfoL.getName()))){
+                msg_name=contactInfoL.getName();
+                msg_number=contactInfoL.getNumber();
+                isMessage=true;
+                return;
+            }
+        }
+        if(res.contains("发短信给")){
+            msg_number=getNumber(res);
+            if(msg_number==null) return;
+            isMessage=true;
+        }
+    }
+
+    //查找
+    private void surfTheInternet(String searchContent) {
+        Intent intent=new Intent(Intent.ACTION_WEB_SEARCH);
+        intent.putExtra(SearchManager.QUERY, searchContent);
+        startActivity(intent);
+    }
+
+    //播放音乐
+    private void playMusic() {
+        if (eventtime<=0)return;
+        Intent downIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+        KeyEvent downEvent = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_PLAY, 0);
+        downIntent.putExtra(Intent.EXTRA_KEY_EVENT, downEvent);
+        sendOrderedBroadcast(downIntent, null);
+
+        Intent upIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+        KeyEvent upEvent = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_UP, KeyEvent.KEYCODE_MEDIA_PLAY, 0);
+        upIntent.putExtra(Intent.EXTRA_KEY_EVENT, upEvent);
+        sendOrderedBroadcast(upIntent, null);
+    }
+
+    //暂停音乐
+    private void pauseMusic() {
+        if (eventtime<=0)return;
+        Intent downIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+        KeyEvent downEvent = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_PAUSE, 0);
+        downIntent.putExtra(Intent.EXTRA_KEY_EVENT, downEvent);
+        sendOrderedBroadcast(downIntent, null);
+
+        Intent upIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+        KeyEvent upEvent = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_UP, KeyEvent.KEYCODE_MEDIA_PAUSE, 0);
+        upIntent.putExtra(Intent.EXTRA_KEY_EVENT, upEvent);
+        sendOrderedBroadcast(upIntent, null);
+    }
+
+    //上一首
+    private void lastMusic() {
+        if (eventtime<=0)return;
+        Intent downIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+        KeyEvent downEvent = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_PREVIOUS, 0);
+        downIntent.putExtra(Intent.EXTRA_KEY_EVENT, downEvent);
+        sendOrderedBroadcast(downIntent, null);
+
+        Intent upIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+        KeyEvent upEvent = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_UP, KeyEvent.KEYCODE_MEDIA_PREVIOUS, 0);
+        upIntent.putExtra(Intent.EXTRA_KEY_EVENT, upEvent);
+        sendOrderedBroadcast(upIntent, null);
+    }
+
+    //下一首
+    private void nextMusic() {
+        if (eventtime<=0)return;
+        Intent downIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+        KeyEvent downEvent = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_NEXT, 0);
+        downIntent.putExtra(Intent.EXTRA_KEY_EVENT, downEvent);
+        sendOrderedBroadcast(downIntent, null);
+
+        Intent upIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+        KeyEvent upEvent = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_UP, KeyEvent.KEYCODE_MEDIA_NEXT, 0);
+        upIntent.putExtra(Intent.EXTRA_KEY_EVENT, upEvent);
+        sendOrderedBroadcast(upIntent, null);
+    }
+}
---
+++ app/src/main/java/com/classmatelin/wakeUpService.java
@@ -0,0 +1,194 @@
+package com.classmatelin;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.Window;
+import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechEvent;
+import com.iflytek.cloud.SpeechUtility;
+import com.iflytek.cloud.VoiceWakeuper;
+import com.iflytek.cloud.WakeuperListener;
+import com.iflytek.cloud.WakeuperResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class wakeUpService extends Service {
+
+    private static final String TAG=wakeUpService.class.getSimpleName();
+    // 语音唤醒对象
+    private VoiceWakeuper mIvw;
+    private int curThresh=1000;
+
+    public wakeUpService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG,"onCreate");
+
+        SpeechUtility.createUtility(wakeUpService.this, SpeechConstant.APPID +"=749f4d3d");
+        // 初始化唤醒对象
+
+    }
+
+    private void startWakeUp(){
+        mIvw = VoiceWakeuper.getWakeuper();
+        if(mIvw!=null){
+            // 清空参数
+            mIvw.setParameter(SpeechConstant.PARAMS, null);
+            // 唤醒门限值，根据资源携带的唤醒词个数按照“id:门限;id:门限”的格式传入
+            mIvw.setParameter(SpeechConstant.IVW_THRESHOLD, "0:" + curThresh);
+            // 设置唤醒模式
+            mIvw.setParameter(SpeechConstant.IVW_SST, "wakeup");
+            // 设置持续进行唤醒
+            mIvw.setParameter(SpeechConstant.KEEP_ALIVE, "1");
+            // 设置闭环优化网络模式
+            mIvw.setParameter(SpeechConstant.IVW_NET_MODE, "0");
+            // 设置唤醒资源路径
+            mIvw.setParameter(SpeechConstant.IVW_RES_PATH, getResource());
+            // 设置唤醒录音保存路径，保存最近一分钟的音频
+            mIvw.setParameter(SpeechConstant.IVW_AUDIO_PATH,
+                    getExternalFilesDir("msc").getAbsolutePath() + "/ivw.wav");
+            mIvw.setParameter(SpeechConstant.AUDIO_FORMAT, "wav");
+
+            mIvw.startListening(mWakeuperListener);
+        }else{
+            Toast.makeText(this,"唤醒未初始化",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //返回结果
+    private WakeuperListener mWakeuperListener = new WakeuperListener() {
+
+        @Override
+        public void onResult(WakeuperResult result) {
+            PackageManager packageManager=wakeUpService.this.getPackageManager();
+
+            try{
+//                String pName=getPackageName();
+//                Intent intent=packageManager.getLaunchIntentForPackage(pName);
+//                startActivity(intent);
+                Intent intent=new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:"+"13531532683"));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+//                Intent intent=new Intent(wakeUpService.this,MainActivity.class);
+//                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                startActivity(intent);
+                Toast.makeText(wakeUpService.this,"启动了",Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                Toast.makeText(wakeUpService.this,"启动未成功，但确实是识别到了",Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onError(SpeechError error) {
+            wakeUpService.super.onCreate();
+        }
+
+        @Override
+        public void onBeginOfSpeech() {
+        }
+
+        @Override
+        public void onEvent(int eventType, int isLast, int arg2, Bundle obj) {
+            switch (eventType) {
+                // EVENT_RECORD_DATA 事件仅在 NOTIFY_RECORD_DATA 参数值为 真 时返回
+                case SpeechEvent.EVENT_RECORD_DATA:
+                    final byte[] audio = obj.getByteArray(SpeechEvent.KEY_EVENT_RECORD_DATA);
+                    Log.i(TAG, "ivw audio length: " + audio.length);
+                    break;
+            }
+        }
+
+        @Override
+        public void onVolumeChanged(int volume) {
+
+        }
+    };
+
+    //资源路径
+    private String getResource() {
+        return "fo|/data/app/com.iflytek.mscv5plusdemo-QYT9BJQwwTuHkmc43e8k3g==/base.apk|176964|987983";
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG,"onStartCommand");
+
+        NotificationManager notificationManager=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+        //构建通知渠道
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel=new NotificationChannel(String.valueOf(R.string.channeled_id),"躺平通知",NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.canBypassDnd();
+            notificationChannel.canShowBadge();
+            notificationChannel.enableVibration(true);
+            notificationChannel.getGroup();
+
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        //构建Notification
+        NotificationCompat.Builder builder=new NotificationCompat.Builder(this.getApplicationContext(), String.valueOf(R.string.channeled_id));
+        Intent intent1=new Intent(this, MainActivity.class);
+        builder.setContentIntent(PendingIntent
+                .getActivities(this, 0,new Intent[]{intent1},0))
+                .setLargeIcon(BitmapFactory.decodeResource(this.getResources(),R.mipmap.tp_small))
+                .setSmallIcon(R.mipmap.tp_small)
+                .setContentTitle("躺屏")
+                .setContentText("躺屏语音唤醒正在后台运行中")
+                .setWhen(System.currentTimeMillis());
+
+        //通知构建
+        Notification notification =builder.build();
+        //设置默认提示音
+        notification.defaults=Notification.DEFAULT_SOUND;
+        //启动图标
+        startForeground(603,notification);
+
+        mIvw = VoiceWakeuper.createWakeuper(this, null);
+        startWakeUp();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG,"onDestroy");
+        stopForeground(true);
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+
+        Log.d(TAG,"onBind");
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
